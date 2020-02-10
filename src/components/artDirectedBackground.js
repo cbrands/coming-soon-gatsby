fd9b2e80e9af3ba69714fc10796f3300436aafd6
Charles Brands <charles.brands@gmail.com>
2020-02-10 14:18:12 +0100

    show picture

--- src/components/artDirectedBackground.js
+++ src/components/artDirectedBackground.js
@@ -50,8 +50,8 @@ const StyledArtDirectedBackground = styled(ArtDirectedBackground)`
   height: 370px;
   max-height: 370px;
   @media (min-width: 800px) {
-    width: 100%;
-    height: 100%;
+    width: 99%;
+    height: 99%;
     max-height: 100vh;
   }
 
